32827c477ca0c854c8e0b33b43aca97f67663454
Rui Vieira <rcardoso@redhat.com>
2023-08-05 22:07:30 +0100

    feat: Delete pods by name

--- examples/01-simple_apply.py
+++ examples/01-simple_apply.py
@@ -1,7 +1,15 @@
 import k8sutils as k8s
 import k8sutils.deployments.utils as deployments
-import k8sutils.pods.utils as pods
+import k8sutils.pods.utils as podsutils
+import k8sutils.pods as pods
 
 k8s.apply("manifests/nginx.yaml")
 deployments.table("nginx")
-pods.table("nginx")
+podsutils.table("nginx")
+
+all_pods = pods.get()
+
+nginx_pods = [pod['metadata']['name'] for pod in all_pods['items'] if 'nginx' in pod['metadata']['name']]
+print(nginx_pods)
+
+pods.delete(nginx_pods[0])
--- k8sutils/pods/__init__.py
+++ k8sutils/pods/__init__.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import subprocess
 
 def get(namespace=None):
@@ -10,3 +11,19 @@ def get(namespace=None):
     result = subprocess.run(cmd, shell=True, check=True, capture_output=True, text=True)
     pods = json.loads(result.stdout)
     return pods
+
+def delete(pod_name, namespace=None):
+    """Delete a specific pod by name"""
+    cmd = f"kubectl delete pod {pod_name}"
+    if namespace:
+        cmd += f" -n {namespace}"
+
+    try:
+        logging.info(f"Deleting pod {pod_name}")
+        subprocess.run(cmd, shell=True, check=True)
+        logging.info(f"Pod {pod_name} deleted successfully")
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Failed to delete pod {pod_name}: {str(e)}")
+        return False
+
+    return True
